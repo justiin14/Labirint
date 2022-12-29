3748eb8a3945994633bc33bcbbdbc63bbb2ea29e
justiin14 <justinianmihai@gmail.com>
2022-12-30 01:21:03 +0200

    Sample labyrinth
    Added fps camera
    Added above camera
    Added box for labyrinth
    Generated labyrinth from code(not file)

---
+++ Labirint/Assets/Labirint.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Labirint : MonoBehaviour
+{
+    public GameObject obiect;
+    public Vector3 pozitie; 
+    public Vector3 zonaIncarcare;
+
+    void Start()
+    {
+        //InstantiereObiect();
+        labirintDeCuburi();
+    }
+
+    public void InstantiereObiect()
+    {
+        pozitie += new Vector3(Random.Range(-zonaIncarcare.x, zonaIncarcare.x), 0,
+        Random.Range(-zonaIncarcare.z, zonaIncarcare.z));
+        Instantiate(obiect, pozitie, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawCube(pozitie, zonaIncarcare);
+    }
+
+    void labirintDeCuburi()
+    {
+        int[,] labirint = {
+                {1, 0, 0, 1, 1, 1},
+                {1, 0, 0, 0, 1, 1 },
+                {1, 0, 0, 0, 0, 1 },
+                {1, 0, 0, 0, 0, 1 },
+                {1, 1, 0, 0, 1, 1 },
+            };
+
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+
+                if (labirint[i, j] == 1)
+                {
+                    Vector3 offset = new Vector3(i + pozitie.x, 0, j + pozitie.z);
+                    Instantiate(obiect, offset, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
